86ff2c15515283d7d20d06c1aecf6c34a18ac87c
blowerywesterville <45107966+blowerywesterville@users.noreply.github.com>
2018-11-16 23:39:06 -0500

    SQL Express database migrations

---
+++ Babysitter1/Migrations/20181115082501_Initial.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Babysitter1.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DefaultSystem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MinTime = table.Column<string>(nullable: true),
+                    MaxTime = table.Column<string>(nullable: true),
+                    Standard = table.Column<decimal>(nullable: false),
+                    Evening = table.Column<decimal>(nullable: false),
+                    LateNite = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DefaultSystem", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Family",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    HomePhone = table.Column<string>(nullable: true),
+                    ContactPhone = table.Column<string>(nullable: true),
+                    ContactEmail = table.Column<string>(nullable: true),
+                    Bedtime = table.Column<string>(nullable: true),
+                    PartialPay = table.Column<bool>(nullable: false),
+                    Cash = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Family", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transaction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FamilyName = table.Column<string>(nullable: true),
+                    JobDate = table.Column<DateTime>(nullable: false),
+                    TotalHours = table.Column<int>(nullable: false),
+                    StartTime = table.Column<DateTime>(nullable: false),
+                    EndTime = table.Column<DateTime>(nullable: false),
+                    Amount = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transaction", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Job",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FamilyId = table.Column<int>(nullable: true),
+                    JobDate = table.Column<DateTime>(nullable: false),
+                    StartTime = table.Column<string>(nullable: true),
+                    EndTime = table.Column<string>(nullable: true),
+                    Paid = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Job", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Job_Family_FamilyId",
+                        column: x => x.FamilyId,
+                        principalTable: "Family",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kid",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Bedtime = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: true),
+                    Note = table.Column<string>(nullable: true),
+                    FamilyId = table.Column<int>(nullable: true),
+                    JobId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kid", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Kid_Family_FamilyId",
+                        column: x => x.FamilyId,
+                        principalTable: "Family",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Kid_Job_JobId",
+                        column: x => x.JobId,
+                        principalTable: "Job",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Job_FamilyId",
+                table: "Job",
+                column: "FamilyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Kid_FamilyId",
+                table: "Kid",
+                column: "FamilyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Kid_JobId",
+                table: "Kid",
+                column: "JobId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DefaultSystem");
+
+            migrationBuilder.DropTable(
+                name: "Kid");
+
+            migrationBuilder.DropTable(
+                name: "Transaction");
+
+            migrationBuilder.DropTable(
+                name: "Job");
+
+            migrationBuilder.DropTable(
+                name: "Family");
+        }
+    }
+}
---
+++ Babysitter1/Migrations/20181115101725_uTransaction.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Babysitter1.Migrations
+{
+    public partial class uTransaction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "Transaction");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "Transaction");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EarlyHours",
+                table: "Transaction",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EveningHours",
+                table: "Transaction",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LateHours",
+                table: "Transaction",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EarlyHours",
+                table: "Transaction");
+
+            migrationBuilder.DropColumn(
+                name: "EveningHours",
+                table: "Transaction");
+
+            migrationBuilder.DropColumn(
+                name: "LateHours",
+                table: "Transaction");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndTime",
+                table: "Transaction",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartTime",
+                table: "Transaction",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
