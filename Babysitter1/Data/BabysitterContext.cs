fc08bc14fa8be75dc50516dd991ade6d047057bb
blowerywesterville <45107966+blowerywesterville@users.noreply.github.com>
2018-11-16 23:33:36 -0500

    Entity Framework data context

---
+++ Babysitter1/Data/BabysitterContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Babysitter1.Models;
+
+namespace Babysitter1.Data
+{
+    public class BabysitterContext : DbContext
+    {
+        public BabysitterContext (DbContextOptions<BabysitterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Job> Job { get; set; }
+
+        public DbSet<Family> Family { get; set; }
+        public DbSet<Kid> Kid { get; set; }
+        public DbSet<DefaultSystem> DefaultSystem { get; set; }
+        public DbSet<Transaction> Transaction { get; set; }
+    }
+}
